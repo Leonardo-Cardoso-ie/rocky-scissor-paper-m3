91d59d1a0000ebe3a22d8faa9e19460090f96fc3
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2022-09-18 16:24:04 +0000

    Fix the identation code is running

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ user_points = 0
 computer_points = 0
 
 
-options = ["r", "s", "p"] #rock, scissor, papers
+options = ["r", "s", "p"]  #rock, scissor, papers list
 
 """Start a loop and use the lower function"""
 while True:
 
     """Use lower() - all characters typed will become lowercase"""
 
-    user_choice = input("Choose R(Rock)/S(Scissor)/P(Paper) ou Q to quit.").lower()
+    user_choice = input("Choose R(Rock)/S(Scissor)/P(Paper) Q to quit").lower()
 
-    if user_choice == 'q':
+    if user_choice == 'q':  #Quit the game
         break
 
     if user_choice not in options:
@@ -24,8 +24,41 @@ while True:
     """Give some skill to the computer"""
 
     cpu_choice = random.randint(0, 2)
-    # 0 : R, 1 : T, 2 : P
-    cpu_option = options[computer_choice]
+    cpu_option = options[cpu_choice]
+
+    print("Computer chose " + cpu_option)  # 0 : R, 1 : T, 2 : P
+
+    if user_choice == cpu_option:
+        print("Tie!")
+
+    elif user_choice == "r" and cpu_option == "s":
+        print("You win!")
+        user_points = user_points + 1
+
+    elif user_choice == "p" and cpu_option == "r":
+        print("You win!")
+        user_points = user_points + 1
+
+    elif user_choice == "S" and cpu_option == "p":
+        print("You win!")
+        user_points = user_points + 1
+
+    """if all conditions are false the user loses the game"""
+
+ 
+print("Your score: " + str(user_points))
+print("Computer score: " + str(computer_points))
+
+if computer_points > user_points:
+    print("You Loose!!!!")
+elif computer_points == user_points:
+    print("Tie")
+else:
+    print("You are the Champion!!")
+
+
+
+
 
 
 
